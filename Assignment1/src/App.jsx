08055cebf5d3f1edbdadb2bceb890d5c75d40397
katakori619 <kmdht9012@gmail.com>
2024-07-27 16:53:54 +0300

    contact semi complete

--- Assignment1/src/App.jsx
+++ Assignment1/src/App.jsx
@@ -6,6 +6,7 @@ import Navbar from './components/Navbar/Navbar'
 import Home from './components/Home/Home'
 import Footer from './components/Footer/Footer'
 import About from './components/About/About'
+import Contact from './components/Contact/Contact'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,7 +15,8 @@ function App() {
     <>
       <Navbar/>
       {/* <Home/> */}
-      <About/>
+      {/* <About/> */}
+      <Contact/>
       <Footer/>
     </>
   )
---
+++ Assignment1/src/components/Contact/Contact.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function Contact() {
+  return (
+    <>
+        <div className="parent3 flex justify-center items-center">
+            <div className="container w-1/2 mt-36 pb-28">
+                <p className='text-4xl font-bold text-center'>CONTACT SECTION</p>
+                <div className="figure my-5 flex justify-center items-center">
+                    <div className="line ms-2"></div>
+                    <i className='fa-solid fa-star mx-3'></i>
+                    <div className="line"></div>
+                </div>
+                <div className="inputs mt-16 flex flex-col items-center">
+                    <input type="text" placeholder='userName' className='my-5 border border-transparent border-b-gray-300 rounded-md w-full p-3'/>
+                    <input type="text" placeholder='userAge' className='my-5 border border-transparent border-b-gray-300 rounded-md w-full p-3'/>
+                    <input type="email" placeholder='userEmail' className='my-5 border border-transparent border-b-gray-300 rounded-md w-full p-3'/>
+                    <input type="password" placeholder='userPassword' className='my-5 border border-transparent border-b-gray-300 rounded-md w-full p-3'/>
+                    <button className='text-white py-2 px-3 self-start rounded-md'>Send Message</button>
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
