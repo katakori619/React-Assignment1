486aedd23e16be59efe5c007a569d694d7884ed6
katakori619 <kmdht9012@gmail.com>
2024-07-27 23:37:54 +0300

    Portfolio completed

--- Assignment1/src/components/Portfolio/Portfolio.jsx
+++ Assignment1/src/components/Portfolio/Portfolio.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React , { useState } from 'react'
 
 export default function Portfolio() {
+
+  const [imgSrc, setImgSrc] = useState('');
+  const [isLayerVisible, setIsLayerVisible] = useState(false);
+  function display(event) {
+    const imgElement = event.currentTarget.querySelector('img');
+    if (imgElement) {
+      setImgSrc(imgElement.src);
+      setIsLayerVisible(true);
+    }
+  }
+
+  function hideLayer() {
+    setIsLayerVisible(false);
+  }
   return (
     <>
       <div className="parent4 flex justify-center items-center">
@@ -13,7 +27,7 @@ export default function Portfolio() {
           </div>
           <div className="photos flex flex-wrap justify-center">
             <div className="w-full sm:w-1/2 lg:w-1/3 rounded-md p-5">
-              <div className="subparent relative overflow-hidden">
+              <div className="subparent relative overflow-hidden" onClick={display}>
                 <img src="https://routeegy.github.io/startFramework/assets/images/poert1.png" className='w-full cursor-pointer rounded-md' alt="" />
                 <div className="layer rounded-md">
                   <i className='text-white fa-solid fa-plus fa-6x'></i>
@@ -21,7 +35,7 @@ export default function Portfolio() {
               </div>
             </div>
             <div className="w-full sm:w-1/2 lg:w-1/3  rounded-md p-5">
-              <div className="subparent relative overflow-hidden">
+              <div className="subparent relative overflow-hidden" onClick={display}>
               <img src="https://routeegy.github.io/startFramework/assets/images/port2.png" className='w-full cursor-pointer rounded-md' alt="" />
                 <div className="layer rounded-md">
                   <i className='text-white fa-solid fa-plus fa-6x'></i>
@@ -29,32 +43,32 @@ export default function Portfolio() {
               </div>
             </div>
             <div className="w-full sm:w-1/2 lg:w-1/3  rounded-md p-5">
-              <div className="subparent relative overflow-hidden">
-                <img src="https://routeegy.github.io/startFramework/assets/images/port3.png" className='w-full cursor-pointer rounded-md relative' alt="" />
+              <div className="subparent relative overflow-hidden" onClick={display}>
+                <img src="https://routeegy.github.io/startFramework/assets/images/port3.png" className='w-full cursor-pointer rounded-md' alt="" />
                 <div className="layer rounded-md">
                   <i className='text-white fa-solid fa-plus fa-6x'></i>
                 </div>
               </div>
             </div>
             <div className="w-full sm:w-1/2 lg:w-1/3  rounded-md p-5">
-              <div className="subparent relative overflow-hidden">
-                <img src="https://routeegy.github.io/startFramework/assets/images/poert1.png" className='w-full cursor-pointer rounded-md relative' alt="" />
+              <div className="subparent relative overflow-hidden" onClick={display}>
+                <img src="https://routeegy.github.io/startFramework/assets/images/poert1.png" className='w-full cursor-pointer rounded-md' alt="" />
                 <div className="layer rounded-md">
                   <i className='text-white fa-solid fa-plus fa-6x'></i>
                 </div>
               </div>
             </div>
             <div className="w-full sm:w-1/2 lg:w-1/3  rounded-md p-5">
-              <div className="subparent overflow-hidden relative">
-                <img src="https://routeegy.github.io/startFramework/assets/images/port2.png" className='w-full cursor-pointer rounded-md relative' alt="" />
+              <div className="subparent overflow-hidden relative" onClick={display}>
+                <img src="https://routeegy.github.io/startFramework/assets/images/port2.png" className='w-full cursor-pointer rounded-md' alt="" />
                 <div className="layer rounded-md">
                   <i className='text-white fa-solid fa-plus fa-6x'></i>
                 </div>
               </div>
             </div>
             <div className="w-full sm:w-1/2 lg:w-1/3  rounded-md p-5">
-              <div className="subparent relative overflow-hidden">
-                <img src="https://routeegy.github.io/startFramework/assets/images/port3.png" className='w-full cursor-pointer rounded-md relative' alt="" />
+              <div className="subparent relative overflow-hidden" onClick={display}>
+                <img src="https://routeegy.github.io/startFramework/assets/images/port3.png" className='w-full cursor-pointer rounded-md' alt="" />
                 <div className="layer rounded-md">
                   <i className='text-white fa-solid fa-plus fa-6x'></i>
                 </div>
@@ -62,6 +76,9 @@ export default function Portfolio() {
             </div>
           </div>
         </div>
+      </div>
+      <div className={isLayerVisible ?'greatlayer fixed top-0 left-0 w-full h-full flex justify-center items-center' :'greatlayer fixed top-0 left-0 w-full h-full justify-center items-center hidden'} onClick={hideLayer}>
+        <img src={imgSrc} alt="" className='w-2/5'/>
       </div>
     </>
   )
