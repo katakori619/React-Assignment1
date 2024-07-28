14e78dcea612e43ff74c9a389ef9845142eec0d1
katakori619 <kmdht9012@gmail.com>
2024-07-28 03:31:32 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/React-Assignment1',
+  base: '/React-Assignment1/',
   plugins: [react()],
 })
